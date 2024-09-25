167ddc1db599503555534ea9a85a608c1e969adb
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-09-25 09:28:40 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -138,7 +138,7 @@ def main():
         codigo_estacao = row['Código']
         
         # Baixa os dados do último mês e obtém a soma
-        soma_ultimo_mes = baixar_dados_estacao(codigo_estacao, sigla_estado, data_inicial_str, data_final_str, login, senha)
+        soma_ultimo_mes = baixar_dados_estacao(codigo_estacao, sigla_estado, data_inicial, data_final, login, senha)
         
         # Adiciona o marcador com a soma do último mês
         m.add_marker(location=[row['Latitude'], row['Longitude']], 
