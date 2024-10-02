edf8113664dc8f5d8191878e53aac36980189f8d
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-10-02 09:09:24 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -100,7 +100,7 @@ def main():
     # Mapa interativo usando Leafmap
     m = leafmap.Map(center=[-18.5122, -44.5550], zoom=7, draw_control=False, measure_control=False, fullscreen_control=False, attribution_control=True)
 
-     Criar um cluster de marcadores para agrupar os marcadores no mapa
+    #Criar um cluster de marcadores para agrupar os marcadores no mapa
     marker_cluster = MarkerCluster().add_to(m)
 
     # Adicionar marcadores das estações meteorológicas em Minas Gerais no estilo fornecido
