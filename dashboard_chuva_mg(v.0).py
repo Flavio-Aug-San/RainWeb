e37607ae0d22154d2b184c8857156335ca5373ea
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-10-02 09:33:46 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -99,8 +99,8 @@ def main():
         unsafe_allow_html=True
     )
 
-    # Mapa interativo usando Leafmap
-    m = leafmap.Map(center=[-18.5122, -44.5550], zoom=7, draw_control=False, measure_control=False, fullscreen_control=False, attribution_control=True)
+    # Criar um cluster de marcadores para agrupar os marcadores no mapa
+    marker_cluster = MarkerCluster().add_to(m)
 
     # Adicionar marcadores das estações meteorológicas em Minas Gerais no estilo fornecido
     for i, row in gdf_mg.iterrows():
@@ -112,8 +112,7 @@ def main():
             fill_color='white',  # Cor de preenchimento
             fill_opacity=0.6,
             popup=f"{row['Nome']} (Código: {row['Código']})"
-        )
-
+        ).add_to(marker_cluster)
     
     # Adiciona o shapefile de Minas Gerais ao mapa sem popups, tooltips ou alterações no cursor
     m.add_gdf(
