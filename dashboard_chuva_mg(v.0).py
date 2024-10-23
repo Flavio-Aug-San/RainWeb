d7c6294bac8b0640a95df22221b64048dbbd2272
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-10-23 10:47:00 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -59,7 +59,7 @@ def baixar_dados_estacao(codigo_estacao, sigla_estado, data_inicial, data_final,
         # junta a tabela que foi lida com a anterior
         dfs = pd.concat([dfs, df0], ignore_index=True)
 
-    soma_selecionada = dfs['valor'].sum()
+    soma_selecionada = dfs['Valor'].sum()
 
 # Função principal do dashboard
 def main():
