a71e80d47ffc983e844f1442c2b3cbc8bf2960b0
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-10-23 11:04:46 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -114,7 +114,7 @@ def main():
             numberOfSides=2,
             rotation=45,
             radius=10,
-            popup=f"{row['municipio']} (Código: {row['codEstacao']})<br>Soma do último mês:{soma_selecionada}"
+            popup=f"{row['municipio']} (Código: {row['codEstacao']})<br>Soma do último mês:"
         ).add_to(m)
 
     m.add_gdf(
