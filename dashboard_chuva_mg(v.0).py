8ed8a44e526650d96342ccfd1990489d29dd2bb8
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-10-25 10:35:47 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -201,7 +201,7 @@ def main():
         mostrar_graficos()
         
     # Mostrar o mapa em Streamlit
-    m.to_streamlit(width=1200,height=775)
+    m.to_streamlit(width=1300,height=875)
     # Chamando a função para exibir o popup
     exibir_popup(chuva_ultima_hora, chuva_ultimas_24_horas, chuva_ultimas_48_horas)
 if __name__ == "__main__":
