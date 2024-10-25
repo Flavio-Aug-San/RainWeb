41c678b6fd2f469110018af0fb70e2104cab5462
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-10-25 09:13:54 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -131,9 +131,7 @@ def main():
     hoje = datetime.now()
     data_inicial = hoje.replace(day=1)
     data_final = hoje
-
-   m = leafmap.Map(center=[-21.5, -45.75], zoom=20, width='100%',height='100%',draw_control=False,measure_control=False,fullscreen_control=True,attribution_control=True)
-
+    m = leafmap.Map(center=[-21.5, -45.75], zoom=20, width='100%',height='100%',draw_control=False,measure_control=False,fullscreen_control=True,attribution_control=True)
     # Adicionar marcadores das estações meteorológicas
     for i, row in gdf_mg.iterrows():
         # Baixar dados da estação
