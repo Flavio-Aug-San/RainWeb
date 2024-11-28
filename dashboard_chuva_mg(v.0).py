b8caa2d5e648962302291a321f08c45d27ad6ec4
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-11-28 16:26:28 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -85,8 +85,9 @@ for ano_mes_dia in pd.date_range(data_inicial, data_final, freq='1M'):
     df.set_index('datahora', inplace=True)
 
     dfuma = df['valor'][-1]
-    df24 = df["valor"].rolling("24h").sum()
-    df48 = df["valor"].rolling("48h").sum()
+    # Filtra e soma os valores das últimas 24 horas
+    soma_ultimas_24h = df[df['data'] >= pd.Timestamp.now() - pd.Timedelta('1D')]['valor'].sum()
+    soma_ultimas_48h = df[df['data'] >= pd.Timestamp.now() - pd.Timedelta('2D')]['valor'].sum()
 
 # Função para exibir gráficos de precipitação
 def mostrar_graficos():
@@ -123,8 +124,8 @@ def exibir_popup(chuva_ultima_hora, chuva_ultimas_24_horas, chuva_ultimas_48_hor
     <div class="popup">
         <h4>Informações de Chuva</h4>
         <p>Chuva na última hora: {dfuma} mm</p>
-        <p>Chuva nas últimas 24 horas: {df24} mm</p>
-        <p>Chuva nas últimas 48 horas: {df48} mm</p>
+        <p>Chuva nas últimas 24 horas: {soma_ultimas_24h} mm</p>
+        <p>Chuva nas últimas 48 horas: {soma_ultimas_48h} mm</p>
     </div>
     """, unsafe_allow_html=True)
     
