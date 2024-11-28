f5465a21aaa01edf15950db29f54b62f37444d8f
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-11-28 09:56:11 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -101,7 +101,7 @@ def baixar_dados_estacao(codigo_estacao, sigla_estado, data_inicial, data_final,
             sws_url = 'http://sws.cemaden.gov.br/PED/rest/pcds/df_pcd'
             params = dict(rede=11, uf=sigla_estado, inicio=ano_mes, fim=ano_mes, codigo=codigo_estacao)
             r = requests.get(sws_url, params=params, headers={'token': token})
-            df_mes = pd.read_csv(pd.compat.StringIO(r.text))
+            df_mes = pd.read_csv(pd.compat(r.text))
             df.append(df_mes)
 
         files = sorted(glob.glob(f'/content/estacao_CEMADEN_{sigla_estado}_{codigo_estacao}*.csv'))
