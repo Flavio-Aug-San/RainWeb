71b0abb935001deea695547cc8138a5c6505a025
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-11-28 10:09:01 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -1,10 +1,9 @@
 import streamlit as st
+import calendar
 import pandas as pd
-import io
 import geopandas as gpd
 import requests
 import numpy as np
-import calendar
 from datetime import datetime, timedelta
 import leafmap.foliumap as leafmap
 import folium
@@ -102,7 +101,7 @@ def baixar_dados_estacao(codigo_estacao, sigla_estado, data_inicial, data_final,
             sws_url = 'http://sws.cemaden.gov.br/PED/rest/pcds/df_pcd'
             params = dict(rede=11, uf=sigla_estado, inicio=ano_mes, fim=ano_mes, codigo=codigo_estacao)
             r = requests.get(sws_url, params=params, headers={'token': token})
-            df_mes = pd.read_csv(io.StringIO(r.text))
+            df_mes = pd.read_csv(pd.compat.StringIO(r.text))
             df.append(df_mes)
 
         files = sorted(glob.glob(f'/content/estacao_CEMADEN_{sigla_estado}_{codigo_estacao}*.csv'))
