eb08ca4292ac69a2c3ec25cec7ac9e056048a319
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-11-28 09:35:33 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -121,9 +121,30 @@ m = leafmap.Map(center=[-21, -45],zoom_start = 8,draw_control=False, measure_con
 # Defina o layout da página como largo
 st.set_page_config(layout="wide")
 
+# Obtém a data e hora atual
+agora = datetime.now()
+
+# Data de hoje
 hoje = datetime.now()
-data_inicial = hoje.replace(day=1)
-data_final = hoje
+
+# Extrai o dia, mês e ano de hoje
+dia_atual = hoje.day
+mes_atual = hoje.month
+ano_atual = hoje.year
+
+# Lógica para o mês anterior
+if mes_atual == 1:  # Caso seja janeiro
+    mes_anterior = 12
+    ano_anterior = ano_atual - 1
+else:
+    mes_anterior = mes_atual - 1
+    ano_anterior = ano_atual
+
+# Primeira e última datas do mês anterior
+diai = '01'
+diaf = str(calendar.monthrange(ano_anterior, mes_anterior)[1])
+data_inicial = f'{ano_anterior}{mes_anterior:02d}{diai}'
+data_final = f'{ano_anterior}{mes_anterior:02d}{diaf}'
 
 # Adicionar marcadores das estações meteorológicas
 for i, row in gdf_mg.iterrows():
