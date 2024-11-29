3163abf7d1193976deaa950ff463b20a60a6fb5e
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-11-29 08:54:39 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -201,7 +201,7 @@ mostrar = st.sidebar.checkbox("Gráfico de Precipitação")
 # Exibir ou ocultar o gráfico conforme o estado do checkbox
 if mostrar:
     mostrar_graficos()
-st.dataframe(df)
+st.dataframe(df_horario)
 
 # Mostrar o mapa em Streamlit
 m.to_streamlit(width=1300,height=775)
