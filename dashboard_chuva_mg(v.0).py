48fbd60f7b62176c2ed82fbfd38e7fd39e55ed46
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-11-29 10:52:46 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -90,10 +90,10 @@ for ano_mes_dia in pd.date_range(data_inicial, data_final, freq='1M'):
     dfuma = df_horario.iloc[-1]
 
     # Filtra e soma os valores das últimas 24 horas
-    soma_ultimas_24h = df_horario.iloc[-24:].sum()
+    soma_ultimas_24h = df_horario['valor'].iloc[-24:].sum()
 
     # Filtra e soma os valores das últimas 48 horas
-    soma_ultimas_48h = df_horario.iloc[-48:].sum()
+    soma_ultimas_48h = df_horario['valor'].iloc[-48:].sum()
 
 # Função para exibir gráficos de precipitação
 def mostrar_graficos():
