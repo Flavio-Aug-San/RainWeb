1a38a88e2b4cd4fb681e7700985a9a6b898634d6
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-12-05 15:50:25 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -24,8 +24,8 @@ senha = 'Flaviobr123!'
 mg_gdf = gpd.read_file(shp_mg_url)
 
 # Estações Selecionadas do Sul de Minas Gerais
-# codigo_estacao = ['314790701A','310710901A','312870901A','315180001A','316930702A','314780801A','315250101A','313240401A','313360001A','311410501A','311360201A','313300601A']
-codigo_estacao = ['312870901A','315180001A']
+codigo_estacao = ['314790701A','310710901A','312870901A','315180001A','316930702A','314780801A','315250101A','313240401A','313360001A','311410501A','311360201A','313300601A']
+
 
 
 # Carregar os dados das estações
@@ -161,10 +161,10 @@ for codigo in list(dados1.keys()):
     if isinstance(valor, pd.DataFrame) and valor.empty:
         del dados1[codigo]  # Remove a chave se for um DataFrame vazio
 dados2 = 0
-#for codigo in dados1.keys():
-  #dados2[codigo] = dados1[codigo][dados1[codigo]['sensor'] != 'intensidade_precipitacao']
-  #dados2[codigo]['datahora'] = pd.to_datetime(dados2[codigo]['datahora'])
-  #dados2[codigo] = dados2[codigo].set_index('datahora')
+for codigo in dados1.keys():
+  dados2[codigo] = dados1[codigo][dados1[codigo]['sensor'] != 'intensidade_precipitacao']
+  dados2[codigo]['datahora'] = pd.to_datetime(dados2[codigo]['datahora'])
+  dados2[codigo] = dados2[codigo].set_index('datahora')
 
 # Adicionar marcadores das estações meteorológicas
 for i, row in gdf_mg.iterrows():    
@@ -227,7 +227,7 @@ if mostrar:
 # Mostrar o mapa em Streamlit
 m.to_streamlit(width=1300,height=775)
 
-st.write(dados1)
+st.json(dados2)
 
 # Chamando a função para exibir o popup
 #exibir_popup(chuva_ultima_hora, chuva_ultimas_24_horas, chuva_ultimas_48_horas)
