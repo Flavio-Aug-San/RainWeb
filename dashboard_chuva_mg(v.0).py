9b5bc1366640a5fcd481b27865cd7d6c1bca5922
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-12-17 19:30:38 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -72,10 +72,10 @@ agora = datetime.now()
 #data_final = pd.to_datetime(data_final)
 
 # 1. Converte a coluna 'datahora' para o formato datetime
-dfoff['datahora'] = pd.to_datetime(dfoff['datahora'], format='%d/%m/%Y %H:%M:%S')
+#dfoff['datahora'] = pd.to_datetime(dfoff['datahora'], format='%d/%m/%Y %H:%M:%S')
 
 # 2. Define a coluna 'datahora' como index
-dfoff.set_index('datahora', inplace=True)
+#dfoff.set_index('datahora', inplace=True)
 
 def baixar_dados_estacoes(codigo_estacao, data_inicial, data_final, sigla_estado):
     # Lista para armazenar os dados de todas as estações
