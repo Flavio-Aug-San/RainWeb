876b5037bdd83da07ff6670a2e789faabbdfb384
Flávio Augusto <50055394+Flavio-Aug-San@users.noreply.github.com>
2024-12-19 10:09:51 -0300

    Update dashboard_chuva_mg(v.0).py

--- dashboard_chuva_mg(v.0).py
+++ dashboard_chuva_mg(v.0).py
@@ -133,7 +133,6 @@ def mostrar_graficos(codigo_estacao, data_inicial):
 
     # ======================== Cálculos de Precipitação ========================
     # Soma do dia atual
-    dia_atual = data_inicial.date()
     soma_dia_atual = dados_estacao[dados_estacao.index.date == dia_atual]['valorMedida'].sum()
     
     # Soma das últimas 24 horas
